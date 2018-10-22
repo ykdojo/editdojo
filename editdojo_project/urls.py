8d7b6fc227028a36b9c150c43efb2c4d62b30a26
Antonio Campos <geckocore03@outlook.com>
2018-10-22 01:23:49 -0500

    Hello world working properly

--- editdojo_project/urls.py
+++ editdojo_project/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from pages.views import homePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sayHello/', homePageView),
+    path('', include('pages.urls')),
 ]
