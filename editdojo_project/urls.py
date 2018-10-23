a3a1c7679294799b2403164759c365e08c8b7977
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2018-10-22 23:29:53 -0400

    making a HelloWorld app

--- editdojo_project/urls.py
+++ editdojo_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hello.views import myView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('say/', myView),
 ]
