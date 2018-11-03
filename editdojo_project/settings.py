58bb5b6b57b11a58265cd359b49e710d6a807a2b
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2018-11-02 22:10:08 -0400

    Make a to-do app (mostly for learning/education purposes)
    https://youtu.be/ovql0Ui3n_I

--- editdojo_project/settings.py
+++ editdojo_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello',
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'editdojo_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- editdojo_project/urls.py
+++ editdojo_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from hello.views import myView
+from todo.views import todoView, addTodo, deleteTodo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sayHello/', myView),
+    path('todo/', todoView),
+    path('addTodo/', addTodo),
+    path('deleteTodo/<int:todo_id>/', deleteTodo),
 ]
