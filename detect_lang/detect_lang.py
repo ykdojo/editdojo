6b469717cd404a1ac92481c7bddcaf47910f45ee
ratdog45 <ratdog45@gmail.com>
2018-11-17 16:30:40 -0500

    Fixed detection issues in detect_lang.py

--- detect_lang/detect_lang.py
+++ detect_lang/detect_lang.py
@@ -1,17 +1,27 @@
-from langdetect import DetectorFactory, detect
-
-# ensures consistent probabilities from detect() method
-DetectorFactory.seed = 0
+from langdetect import DetectorFactory, detect, detect_langs
+import time
 
 def get_message_lang(message_text, user_target_lang, user_native_lang):
-    if detect(message_text) == user_target_lang:
-        return user_target_lang
-    elif detect(message_text) == user_native_lang:
-        return user_native_lang
-    else:
-        # Currently if a message has a number of spelling errors, the langdetect
-        # library will possibly misidentify the language; i.e. "Helo Woorld"
-        # will be misidentified as Dutch, rather than English. Returning the
-        # entered message in this situation is just a placeholder until this
-        # issue is resolved.
-        return message_text
+    try:
+        # return target lang if message is target lang
+        if detect(message_text) == user_target_lang:
+            return user_target_lang
+
+        # return native lang if message is native lang
+        elif detect(message_text) == user_native_lang:
+            return user_native_lang
+
+        # if native/target lang is not detected (due to langdetect probability algorithm) then generate new probabilities
+        # until native/target lang is detected
+        else:
+            init_time = time.time()
+            while True:
+                # if native/target lang is not detected after five seconds, return None
+                if (time.time()-init_time) >= 5:
+                    return None
+                prob_list = detect_langs(message_text)
+                for item in prob_list:
+                    if item.lang == user_target_lang or item.lang == user_native_lang:
+                        return item.lang
+    except:
+        return None
