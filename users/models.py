3ac1ca312ca991b0c34ccea0de044c7d10a2d4e1
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2018-11-26 16:28:56 -0500

    Created a custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+class CustomUserManager(UserManager):
+    pass
+
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
