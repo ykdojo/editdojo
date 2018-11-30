272737dee8d6eb6bfc74bb214e3cbf7ce62a3e0c
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2018-11-30 16:38:19 -0500

    When a user signs up, follow them from @EditDojo

--- users/views.py
+++ users/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import tweepy
+from .keys import *
 
 def login(request):
+    username = None
+    if not request.user.is_authenticated:
+        return render(request, 'login.html')
+
+    username = request.user.username
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    api.create_friendship(username)
+    # TODO: store some info about if we already follow them or not
     return render(request, 'login.html')
