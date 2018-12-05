2f58d0dda796f1427c7cea89975881e7360fbd73
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2018-12-04 20:34:15 -0500

    Use a user's Twitter ID to follow them instead of their Twitter handle.

--- users/views.py
+++ users/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import tweepy
-from .keys import *
+from .keys import * # this is for tweepy
+from allauth.socialaccount.models import SocialAccount
 
 def login(request):
     username = None
-    if not request.user.is_authenticated:
+    current_user = request.user
+    if not current_user.is_authenticated:
         return render(request, 'login.html')
 
-    username = request.user.username
+    twitter_account = SocialAccount.objects.filter(user = current_user)[0]
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
     api = tweepy.API(auth)
-    api.create_friendship(username)
-    # TODO: store some info about if we already follow them or not
+    api.create_friendship(twitter_account.uid)
+    # TODO: store some info about if we already follow them or not.
     return render(request, 'login.html')
