f35aa9ce282a5b29c25b4ec467478a19b61a1d6a
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2018-12-10 21:57:51 -0500

    Added Language to admin

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Language
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
@@ -11,3 +11,4 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Language)
