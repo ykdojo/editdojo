ec33005f7443abd4cd7c1012487d6f755ece3048
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2018-12-16 14:12:49 -0500

    Set up STATIC_ROOT

--- editdojo_project/settings.py
+++ editdojo_project/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
